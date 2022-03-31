54bcb3237f8d190b0cc60ffcd4c030bbdb70b69b
André Rodrigues <andre.rodrigues2@sky.uk>
2022-03-31 17:06:22 +0100

    added about us

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,7 @@ import Home from "pages/Home/Home";
 import Shop from "pages/Shop/Shop";
 import Cart from "pages/ShoppingCart/Cart";
 import ShoppingItem from "pages/ShoppingItem/ShoppingItem";
+import AboutUs from "pages/AboutUs/AboutUs";
 import { Route, Routes } from "react-router";
 import "react-slideshow-image/dist/styles.css";
 import "styles/global.scss";
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
         <Route path="/shoppingCart" element={<Cart />} />
         <Route path="/shoppingItem:id" element={<ShoppingItem />} />
         <Route path="*" element={<Home />} />
